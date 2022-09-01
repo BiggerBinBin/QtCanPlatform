d11eb756c4c8f804623bb572fa40f9774252722a
BiggerBinBin <1091313282@qq.com>
2022-09-01 23:51:12 +0800

    新增属性vector，删除属性map

--- dataStruct.h
+++ dataStruct.h
@@ -3,18 +3,28 @@
 #include <iostream>
 #include<vector>
 #include <QColor>
+struct YBRGB
+{
+	uchar r;
+	uchar g;
+	uchar b;
+};
 struct parseData
 {
 	QString name;
 	int value;
+	QString toWord;
+	struct YBRGB color;
 };
 struct cellProperty
 {
-	QString toWord;
-	//QColor  color;
 	uint16_t r;
 	uint16_t g;
 	uint16_t b;
+	QString toWord;
+	QString value;
+	//QColor  color;
+	
 };
 struct protoItem {
 	int send;
@@ -24,7 +34,8 @@ struct protoItem {
 	int precision;
 	int offset;
 	QString bitName;
-	std::map<QString, cellProperty>itemProperty;
+	std::vector<cellProperty>stl_itemProperty;
+	//std::map<QString, cellProperty>itemProperty;
 };
 struct canIdData {
 	uint8_t opt;
